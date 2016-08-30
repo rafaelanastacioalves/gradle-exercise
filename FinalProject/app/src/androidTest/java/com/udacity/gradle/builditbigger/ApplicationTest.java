746acedcaeff81cae32e3933b8cfcd46a52963fa
Rafael  Alves <rafael.alves@concretesolutions.com.br>
2016-08-30 18:07:54 -0300

    Step 4 done

--- FinalProject/app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
+++ FinalProject/app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
@@ -1,13 +1,54 @@
 package com.udacity.gradle.builditbigger;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.content.Context;
+import android.support.v4.util.Pair;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.MediumTest;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.concurrent.ExecutionException;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ * Credits to the solution of exercise 4.10 from Udacity's Gradle Course.
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
+public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private MainActivity mMainActivity;
+    private Button mButton;
+    private TextView mTextView;
+
     public ApplicationTest() {
-        super(Application.class);
+        super(MainActivity.class);
     }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        setActivityInitialTouchMode(true);
+        mMainActivity = getActivity();
+
+    }
+
+    @MediumTest
+    public void testVerifyEchoResponse() {
+        EndpointsAsyncTask asyncTaskCompat = new EndpointsAsyncTask();
+        asyncTaskCompat.execute(new Pair<Context, String>(mMainActivity, "whathever"));
+        try {
+            assertNotNull("It is null!", (String) asyncTaskCompat.get());
+            assertNotSame("It's empty!", "", asyncTaskCompat.get());
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+
 }
