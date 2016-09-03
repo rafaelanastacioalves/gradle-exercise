411cb08e69351fd9c078b09435b13166348c98ac
Rafael Anastacio Alves <rafaelanastacioalves@gmail.com>
2016-09-03 10:43:03 -0300

    Refactor so tests be more efficient.

--- FinalProject/app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
+++ FinalProject/app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
@@ -1,16 +1,11 @@
 package com.udacity.gradle.builditbigger;
 
 import android.support.test.runner.AndroidJUnit4;
-
 import android.test.suitebuilder.annotation.SmallTest;
 import android.util.Log;
 
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-
-import java.util.concurrent.ExecutionException;
 
 import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertNotSame;
@@ -28,32 +23,19 @@ public class ApplicationTest {
     private final String LOG_TAG = getClass().getCanonicalName();
 
     @Test
-    public void testVerifyEchoResponse() {
+    public void testVerifyAsyncResponse() {
         EndpointsAsyncTask asyncTaskCompat = new EndpointsAsyncTask(new EndpointsAsyncTaskCallBack () {
             @Override
             public void onTaskSuccess(String result) {
 
                     assertNotNull("It is null!", result);
-                    assertNotSame("It's empty!", result, result);
+                    assertNotSame("It's empty!", "", result);
                 Log.i(LOG_TAG,"Checked via interface. The result was: " + result);
 
             }
         });
 
         asyncTaskCompat.execute();
-
-        try {
-            Log.i(LOG_TAG,"Before checking via direct run...");
-            assertNotNull("It is null!", (String) asyncTaskCompat.get());
-            assertNotSame("It's empty!", "", asyncTaskCompat.get());
-            Log.i(LOG_TAG,"Checked via direct run. The result was: " + asyncTaskCompat.get());
-
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
 
 
     }
